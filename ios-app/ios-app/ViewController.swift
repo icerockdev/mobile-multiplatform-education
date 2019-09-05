79ed814c04063fc7d9e1a974b05bd74d30058f64
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-09-05 16:20:27 +0700

    initial commit

---
+++ ios-app/ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-app
+//
+//  Created by Aleksey Mikhailov on 05/09/2019.
+//  Copyright © 2019 IceRock Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
