43504c8598d4ccbf9ddd530e818e8c2481167986
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-09-05 16:44:31 +0700

    l1-s14 add ios specific hello world

---
+++ mpp-library/src/iosMain/kotlin/IosHelloWorld.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import platform.Foundation.NSLog
+
+object IosHelloWorld {
+    fun print() {
+        NSLog("hello ios world")
+    }
+}
