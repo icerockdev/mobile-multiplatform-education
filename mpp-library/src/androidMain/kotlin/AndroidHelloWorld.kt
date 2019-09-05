5f52ed8e241d960bd27c70a7880ab9f544ececc1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-09-05 16:40:42 +0700

    l1-s12 add android specific hello world

---
+++ mpp-library/src/androidMain/kotlin/AndroidHelloWorld.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import android.util.Log
+
+object AndroidHelloWorld {
+    fun print() {
+        Log.v("MPP", "hello android world")
+    }
+}
