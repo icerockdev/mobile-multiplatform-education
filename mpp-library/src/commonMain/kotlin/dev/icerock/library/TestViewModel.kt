e51698352cf77d179e8de1ac85e824522b5f6fef
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-09-12 17:28:00 +0700

    l4-s3 change logic of viewmodel to even/odd label showing

--- mpp-library/src/commonMain/kotlin/dev/icerock/library/TestViewModel.kt
+++ mpp-library/src/commonMain/kotlin/dev/icerock/library/TestViewModel.kt
@@ -1,5 +1,7 @@
 package dev.icerock.library
 
+import dev.icerock.moko.mvvm.desc.StringDesc
+import dev.icerock.moko.mvvm.desc.desc
 import dev.icerock.moko.mvvm.livedata.LiveData
 import dev.icerock.moko.mvvm.livedata.MutableLiveData
 import dev.icerock.moko.mvvm.livedata.map
@@ -9,9 +11,15 @@ import dev.icerock.moko.mvvm.viewmodel.ViewModel
  * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-class TestViewModel: ViewModel() {
+class TestViewModel : ViewModel() {
     private val _counter: MutableLiveData<Int> = MutableLiveData(0)
-    val counter: LiveData<String> = _counter.map { it.toString() }
+    val counter: LiveData<StringDesc> = _counter.map {
+        if (it % 2 == 0) {
+            MR.strings.even
+        } else {
+            MR.strings.odd
+        }.desc()
+    }
 
     fun onUpButtonPressed() {
         _counter.value = _counter.value + 1
